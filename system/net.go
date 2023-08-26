a288f387f93c192ae42cb0e135cc72c333be5ba8
Vassilios Karakoidas <bkarak@e-food.gr>
2023-08-26 23:56:08 +0300

    using resty

---
+++ system/net.go
@@ -0,0 +1,9 @@
+package system
+
+func Get(url string, headers map[string]string) interface{} {
+	return new(interface{})
+}
+
+func Post(url string, payload map[string]string, headers map[string]string) interface{} {
+	return new(interface{})
+}
